bcbfd77b47ffda5632a5c7addca0bd81d5def305
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-10-03 06:23:38 -0600

    Wrote guide.

--- mdev.sh
+++ mdev.sh
@@ -1,9 +1,16 @@
-#Quadro 1 32e17d6f-ca09-427d-b378-9632a137f4c6
-#Quadro 2 267ad360-1f1c-46a9-9ba9-31bf7d890d33
 # nvidia-48 -> GRID P40-3Q
+VGPU_TYPE=nvidia-48
 
-mdevctl start -u 32e17d6f-ca09-427d-b378-9632a137f4c6 -p 0000:2b:00.0 --type nvidia-48
-mdevctl start -u 267ad360-1f1c-46a9-9ba9-31bf7d890d33 -p 0000:2b:00.0 --type nvidia-48
+PCI_ADDRESS=$(lspci | grep VGA | grep -Po '\d\w:\d\d.\d')
 
-mdevctl define --auto --uuid 32e17d6f-ca09-427d-b378-9632a137f4c6
-mdevctl define --auto --uuid 267ad360-1f1c-46a9-9ba9-31bf7d890d33
+#Quadro 1
+UUID1=$(uuidgen)
+
+#Quadro 2
+UUID2=$(uuidgen)
+
+mdevctl start -u $UUID1 -p 0000:$PCI_ADDRESS -t $VGPU_TYPE
+mdevctl start -u $UUID2 -p 0000:$PCI_ADDRESS -t $VGPU_TYPE
+
+mdevctl define -a -u $UUID1
+mdevctl define -a -u $UUID2
